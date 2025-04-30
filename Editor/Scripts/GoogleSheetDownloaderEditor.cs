b6558fad525ca3985c2ea8d2a4b70cc856b88d92
Varuzh29 <52662133+Varuzh29@users.noreply.github.com>
2025-04-30 14:10:16 +0300

    Update GoogleSheetDownloaderEditor.cs

--- Editor/Scripts/GoogleSheetDownloaderEditor.cs
+++ Editor/Scripts/GoogleSheetDownloaderEditor.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 namespace VarCo.GoogleSheetsDownloader
 {
     [CustomEditor(typeof(GoogleSheetDownloader))]
-    public class GoogleSheetDownloaderEditor : Editor
+    public class GoogleSheetDownloaderEditor : UnityEditor.Editor
     {
         public override void OnInspectorGUI()
         {
